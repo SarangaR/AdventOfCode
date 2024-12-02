8eadf7ab40f6cfb91939c6490b7d71f425e1dd6f
SarangaR <sarnga.raj@gmail.com>
2024-12-02 15:29:21 -0500

    template

---
+++ template/src/main.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+use std::{fs::File, io::BufReader, path::PathBuf, process::exit};
+
+fn main() {
+    let input = BufReader::new(File::open(Args::parse().input).unwrap_or_else(|err| die(err)));
+}
+
+fn die<T: ToString>(err: T) -> ! {
+    eprintln!("Error: {}", err.to_string());
+    exit(1);
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to the input file
+    input: PathBuf,
+}
